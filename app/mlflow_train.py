2e29455bbed1e61f8e8241764b546f3310b0059c
arthurcornelio <arthur.cornelio@gmail.com>
2025-01-10 13:12:28 +0100

    mlflow experiment running, grid search reduced

--- app/mlflow_train.py
+++ app/mlflow_train.py
@@ -1,10 +1,10 @@
 from app.pipeline import process_and_pipeline
 from app.data_preprocessing import load_data
-from app.model import create_model
+from app.model import Model
 from sklearn.model_selection import GridSearchCV
 import mlflow
 import time
-
+import pandas as pd
 
 def train_model(X_train_processed, y_train, param_grid, cv=2, n_jobs=-1, verbose=3):
     """
@@ -19,10 +19,12 @@ def train_model(X_train_processed, y_train, param_grid, cv=2, n_jobs=-1, verbose
     Returns:
         GridSearchCV: Trained GridSearchCV object.
     """
-    tree_model = create_model()
+
+    tree_model = Model()
+    print(tree_model.get_params().keys())
     gs_cv_model = GridSearchCV(tree_model, param_grid, n_jobs=n_jobs, verbose=verbose, cv=cv, scoring="r2")
-    gs_cv_model.fit(X_train_processed, y_train)
-    return gs_cv_model
+    gs_cv_model_fitted = gs_cv_model.fit(X_train_processed, y_train)
+    return gs_cv_model_fitted
 
 # Log metrics and model to MLflow
 def log_metrics_and_model(model, X_train, y_train, X_test, y_test, artifact_path, registered_model_name):
@@ -37,6 +39,9 @@ def log_metrics_and_model(model, X_train, y_train, X_test, y_test, artifact_path
         artifact_path (str): Path to store the model artifact.
         registered_model_name (str): Name to register the model under in MLflow.
     """
+
+    y_train = pd.DataFrame(y_train)
+
     mlflow.log_metric("Train Score", model.score(X_train, y_train))
     mlflow.log_metric("Test Score", model.score(X_test, y_test))
     mlflow.sklearn.log_model(
@@ -63,7 +68,7 @@ def run_experiment(experiment_name, data_file, param_grid, artifact_path, regist
 
     # Load, preprocess data and create pipeline
     df_raw = load_data(data_file)
-    X_train, X_test, y_train, y_test, pipeline = process_and_pipeline(df_raw)
+    X_train, y_train, X_test, y_test, pipeline = process_and_pipeline(df_raw, strat=True)
 
     # Set experiment's info 
     mlflow.set_experiment(experiment_name)
@@ -76,10 +81,10 @@ def run_experiment(experiment_name, data_file, param_grid, artifact_path, regist
 
     with mlflow.start_run(experiment_id=experiment.experiment_id):
         # Train model
-        model = train_model(X_train, y_train, param_grid)
+        best_model = train_model(X_train, y_train, param_grid).best_estimator_
 
         # Log metrics and model to MLflow
-        log_metrics_and_model(model, X_train, y_train, X_test, y_test, artifact_path, registered_model_name)
+        log_metrics_and_model(best_model, X_train, y_train, X_test, y_test, artifact_path, registered_model_name)
 
     # Print timing
     print(f"...Training Done! --- Total training time: {time.time() - start_time} seconds")
@@ -91,11 +96,11 @@ if __name__ == "__main__":
     model_folder = '/home/models/'  # Update path to match container structure
 
     param_grid = {
-        "max_depth": [3, 5, 10, 15],  # Profondeurs d'arbre à tester
-        "min_samples_split": [2, 5, 10, 20],  # Minimum pour diviser un nœud
-        "min_samples_leaf": [1, 2, 4, 8],  # Minimum d'échantillons dans une feuille
-        "criterion": ["squared_error", "absolute_error"],  # Fonction de perte
-        "max_features": [None, "sqrt", "log2"]  # Options de sélection des caractéristiques
+        "max_depth": [3, 5],  # Profondeurs d'arbre à tester
+        "min_samples_split": [5, 20],  # Minimum pour diviser un nœud
+        "min_samples_leaf": [2, 8],  # Minimum d'échantillons dans une feuille
+        "criterion": ["absolute_error"],  # Fonction de perte
+        "max_features": [None, "sqrt"]  # Options de sélection des caractéristiques
     }
 
     artifact_path = "modeling_airbnb_pricing"
